8aba1f6f157b96162b0d7243abf11b42693d2cdb
Samuel Megbenu <samuelmegbenu1@gmail.com>
2024-09-23 12:12:04 +0000

    imported os

--- ollama_project/settings.py
+++ ollama_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-jnsjy8u!7y=ay8f*c4&390n)94-y_i-2f3x!l3s6(!du-33g4w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ollama-project.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'ollama-project.onrender.com').split(',')
 
 
 # Application definition
